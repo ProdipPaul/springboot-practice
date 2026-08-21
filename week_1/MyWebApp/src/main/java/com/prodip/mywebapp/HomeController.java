5aed8ec3ed9ff071d14615077c8065490787038b
ProdipPaul <prodippaul181@gmail.com>
2026-08-22 03:18:19 +0600

    Updated MyWebApp project code

--- week_1/MyWebApp/src/main/java/com/prodip/mywebapp/HomeController.java
+++ week_1/MyWebApp/src/main/java/com/prodip/mywebapp/HomeController.java
@@ -1,11 +1,5 @@
 /*
 
-package com.prodip.mywebapp;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 @Controller
 public class HomeController {
 
@@ -17,24 +11,21 @@ public class HomeController {
     }
 
 }
-
  */
+
 package com.prodip.mywebapp;
 
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
 
+/* =============================================================
     @RequestMapping("home")
-    //@ResponseBody // এটি দিলে কোনো JSP বা ভিউ ফাইল খুঁজবে না
     public String home(HttpServletRequest req, HttpServletResponse res)
     {
         HttpSession session = req.getSession();
@@ -44,4 +35,20 @@ public class HomeController {
         session.setAttribute("name", name);
         return "home";
     }
+    ============================================================= */
+
+    //Update_Code
+
+    @RequestMapping("home")
+    public ModelAndView home(@RequestParam("name") String myName)
+    {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("name", myName);
+        mv.setViewName("home");
+
+        return mv;
+    }
 }
+
+
+
