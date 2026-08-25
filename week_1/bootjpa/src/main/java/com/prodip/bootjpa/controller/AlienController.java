ac5c10c3979df4cf7209d832a0ca5a66bade307b
ProdipPaul <prodippaul181@gmail.com>
2026-08-25 21:19:57 +0600

    Updated running project code

--- week_1/bootjpa/src/main/java/com/prodip/bootjpa/controller/AlienController.java
+++ week_1/bootjpa/src/main/java/com/prodip/bootjpa/controller/AlienController.java
@@ -1,4 +1,15 @@
 package com.prodip.bootjpa.controller;
 
-public class AlienController {
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AlienController
+{
+    @RequestMapping("/")
+    public String home()
+    {
+        return "home.jsp";
+    }
 }
