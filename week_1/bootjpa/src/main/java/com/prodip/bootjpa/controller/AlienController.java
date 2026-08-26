11ddb1937a83ca544678886a912304c254591925
ProdipPaul <prodippaul181@gmail.com>
2026-08-26 21:26:06 +0600

    update code  Rest Api

--- week_1/bootjpa/src/main/java/com/prodip/bootjpa/controller/AlienController.java
+++ week_1/bootjpa/src/main/java/com/prodip/bootjpa/controller/AlienController.java
@@ -5,8 +5,10 @@ import com.prodip.bootjpa.dao.AlienRepo;
 import com.prodip.bootjpa.model.Alien;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -28,20 +30,13 @@ public class AlienController
         return "home.jsp";
     }
 
-    @RequestMapping("/getAlien")
-    public ModelAndView getAlien(@RequestParam int aid)
+    @RequestMapping("/alien/{aid}")
+    @ResponseBody
+    public String getAliens(@PathVariable int aid)
     {
-        ModelAndView mv = new ModelAndView("showAlien.jsp");
-        Alien alien  = repo.findById(aid).orElse(new Alien());
 
+        return  repo.findById(aid).toString();
 
-        System.out.println(repo.findByTech("Java"));
-        System.out.println(repo.findByAidGreaterThan(102));
-        System.out.println(repo.findByTechSorted("Java"));
-
-
-        mv.addObject(alien);
-        return mv;
     }
 
 }
