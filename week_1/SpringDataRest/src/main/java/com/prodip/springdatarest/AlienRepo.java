19eb3cbe94dc5929c96e569a361c86bb0223342c
ProdipPaul <prodippaul181@gmail.com>
2026-08-29 21:57:38 +0600

    Rest data, delete , put , patch, etc

---
+++ week_1/SpringDataRest/src/main/java/com/prodip/springdatarest/AlienRepo.java
@@ -0,0 +1,12 @@
+package com.prodip.springdatarest;
+
+import com.prodip.springdatarest.model.Alien;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+
+@RepositoryRestResource(collectionResourceRel = "aliens", path="aliens")
+public interface AlienRepo extends JpaRepository<Alien, Integer>
+{
+
+}
---
+++ week_1/SpringDataRest/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values (101,'Prodip Paul','Java');
+insert into alien values (102,'Shojib' ,'Android');
+insert into alien values (103,'Ovojit','Python');
+insert into alien values (104,'Dipu','Java');
+insert into alien values (105,'Rahul','Android');
